4027064cb200855d69cfa77453bec9d6097ce44f
Sai-Sathwik2718 <158552902+Sai-Sathwik2718@users.noreply.github.com>
2025-12-07 17:43:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Isomorphic Strings/isomorphic-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean areIsomorphic(String s1, String s2) {
+        // code here
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+        HashMap<Character,Character> map1=new HashMap<>();
+        HashSet<Character> set=new HashSet<>();
+        for(int i=0;i<s1.length();i++){
+            char ch1=s1.charAt(i);
+            char ch2=s2.charAt(i);
+            if(map1.containsKey(ch1)){
+                
+                if(map1.get(ch1)!=ch2) return false;
+            }else{
+                if(set.contains(ch2)) return false;
+                map1.put(ch1,ch2);
+                set.add(ch2);
+            }
+                
+        }
+        return true;
+    }
+}
