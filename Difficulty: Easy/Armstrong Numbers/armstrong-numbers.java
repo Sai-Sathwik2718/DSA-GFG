a6364d2f27c09b6d7792d37a6face26dd14adcd2
Sai-Sathwik2718 <158552902+Sai-Sathwik2718@users.noreply.github.com>
2025-12-31 11:43:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
@@ -0,0 +1,15 @@
+// User function Template for Java
+class Solution {
+    static boolean armstrongNumber(int n) {
+        // code here
+        int org=n;
+        int sum=0;
+        int digits=String.valueOf(n).length();
+        while(n!=0){
+            int digit=n%10;
+            sum+=Math.pow(digit,digits);
+            n=n/10;
+        }
+        return sum==org;
+    }
+}
